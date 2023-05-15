721aa3585e7e547919ebb3def1df3d358555240a
Byron Tercero <B1tercero.cruz@gmail.com>
2023-05-14 23:31:25 -0600

    I am working with Path

--- CSharp 10/Modulo 11 - Trabajando con Archivos/Archivos/Program.cs
+++ CSharp 10/Modulo 11 - Trabajando con Archivos/Archivos/Program.cs
@@ -1,28 +1,18 @@
-﻿var rutaEjemplo = @"F:\Documentos\REPOSITORIOS\.NET\Curso_CSharp\Programando-En-Csharp-Udemy\CSharp 10\Modulo 11 - Trabajando con Archivos\Archivos";
+﻿var rutaEjemplo = @"F:\Documentos\REPOSITORIOS\.NET\Curso_CSharp\Programando-En-Csharp-Udemy\CSharp 10\Modulo 11 - Trabajando con Archivos\Archivos\";
 
-//Directory.CreateDirectory(ruta); // crea un directorio
-
-// crea un enumerable de strings que contiene todas las carpetas del directorio
-//var rutas = Directory.EnumerateDirectories(rutaEjemplo);
-//var rutas = Directory.EnumerateDirectories(rutaEjemplo, "*", SearchOption.AllDirectories);
-
-//var rutas = Directory.EnumerateFiles(rutaEjemplo, "*", SearchOption.AllDirectories);
-var rutas = Directory.EnumerateFiles(rutaEjemplo, "*.json", SearchOption.AllDirectories);
+var rutas = Directory.EnumerateDirectories(rutaEjemplo, "*", SearchOption.AllDirectories);
 
 foreach (var ruta in rutas)
 {
-    Console.WriteLine(ruta);
+    var nombreArchivo = Path.GetFileName(ruta); // obtiene la nombre de la ruta
+    var extension = Path.GetExtension(ruta); // obtiene la extension  de los archivos
+    Console.WriteLine($"{nombreArchivo} - extension: {extension}");
 }
 
-var rutaBorrar = @"F:\Documentos\REPOSITORIOS\.NET\Curso_CSharp\Programando-En-Csharp-Udemy\CSharp 10\Modulo 11 - Trabajando con Archivos\Archivos\Mi Directorio 2\sub directorio";
+//combinar rutas
 
+var ruta1 = @"C:\user\source";
+var ruta2 = @"repos\archivos";
 
-if (Directory.Exists(rutaBorrar))
-{
-    Directory.Delete(rutaBorrar);
-    Console.WriteLine("Se ha borrado el directorio");
-}
-else
-{
-    Console.WriteLine("No se borro nada");
-}
+var rutasCombinadas = Path.Combine(ruta1, ruta2);
+Console.WriteLine(rutasCombinadas);
