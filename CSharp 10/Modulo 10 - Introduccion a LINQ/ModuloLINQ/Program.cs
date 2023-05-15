c0289ed9042124a399da1a20b243908491075f3b
Byron Tercero <B1tercero.cruz@gmail.com>
2023-05-14 22:19:43 -0600

    Leer, copiar y eliminar archivos

--- CSharp 10/Modulo 10 - Introduccion a LINQ/ModuloLINQ/Program.cs
+++ CSharp 10/Modulo 10 - Introduccion a LINQ/ModuloLINQ/Program.cs
@@ -1,26 +1,29 @@
 ﻿using ModuloLINQ;
 using System.ComponentModel.DataAnnotations.Schema;
 
-var personas = new List<Persona>
+
+
+int DiagonalDifference(List<List<int>> arr)
 {
-    new Persona {Nombre = "Byron Tercero", Edad = 20, Soltero = true, EmpresaId = 1, FechaIngresoALaEmpresa = DateTime.Now},
-    new Persona {Nombre = "Jose Cruz", Edad = 25, Soltero = true, EmpresaId = 2, FechaIngresoALaEmpresa = DateTime.Now},
-    new Persona {Nombre = "Gabriela Rocha", Edad = 27, Soltero = false, EmpresaId = 3, FechaIngresoALaEmpresa = new DateTime(2022, 4, 12)},
-    new Persona {Nombre = "Rodolfo Alto", Edad = 23, Soltero = true, EmpresaId = 4, FechaIngresoALaEmpresa = new DateTime(2022, 6, 11)}
+    int primarySum = 0;
+    int secondarySum = 0;
+
+    for (int i = 0; i < arr.Count; i++)
+    {
+        primarySum += arr[i][i];
+        secondarySum += arr[i][arr.Count - 1 - i];
+    }
+    int diagonalDifference = Math.Abs(primarySum - secondarySum);
+    return diagonalDifference;
+}
+
+
+List<List<int>> matrix = new List<List<int>>()
+{
+    new List<int> {11, 2, 4 },
+    new List<int> {4, 5, 6 },
+    new List<int> {10, 8, -12 }
 };
 
-//foreach (var persona in personas.OrderBy(p => p.Edad))
-//{
-//    Console.WriteLine($"{persona.Nombre} tiene una edad de {persona.Edad}");
-//}
-
-// ejemplo 2
-
-int[] numeros = { 1, 4, 5, 7, 2, 3, 8, 10, 9 };
-
-var numerosOrdenados = numeros.OrderBy(n => n).ToList();
-
-foreach (var numero in numerosOrdenados)
-{
-    Console.WriteLine(numero);
-}
+int resultado = DiagonalDifference(matrix);
+Console.WriteLine(resultado);
--- CSharp 10/Modulo 11 - Trabajando con Archivos/Archivos/Program.cs
+++ CSharp 10/Modulo 11 - Trabajando con Archivos/Archivos/Program.cs
@@ -1,1 +1,20 @@
-﻿// El código de este módulo se encuentra en la carpeta 'Codigo'
+﻿var ruta = @"F:\Documentos\REPOSITORIOS\.NET\Curso_CSharp\Programando-En-Csharp-Udemy\CSharp 10\Modulo 11 - Trabajando con Archivos\Archivos\ejemplo.txt";
+
+var rutaDestino = @"F:\Documentos\REPOSITORIOS\.NET\Curso_CSharp\Programando-En-Csharp-Udemy\CSharp 10\Modulo 11 - Trabajando con Archivos\Archivos\ejemplo copiado.txt";
+
+if (File.Exists(ruta))
+{
+    var contenido = File.ReadAllText(ruta); // lee el archivo
+    Console.WriteLine(contenido);
+    // copia el archivo y lo sobre escribe
+    File.Copy(ruta, rutaDestino, overwrite: true);
+}
+else
+{
+    Console.WriteLine("No existe el archivo");
+}
+
+// elemina el archivo destino
+File.Delete(rutaDestino);
+
+Console.WriteLine();
