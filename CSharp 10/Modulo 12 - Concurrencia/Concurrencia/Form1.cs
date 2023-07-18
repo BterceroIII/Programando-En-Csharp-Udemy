dbb39de41d65038acce2645354fe8c356574e547
Byron Tercero <B1tercero.cruz@gmail.com>
2023-07-17 21:48:36 -0600

    Utilizando Lock

--- CSharp 10/Modulo 12 - Concurrencia/Concurrencia/Form1.cs
+++ CSharp 10/Modulo 12 - Concurrencia/Concurrencia/Form1.cs
@@ -13,18 +13,36 @@ namespace Concurrencia
         private  void button1_Click(object sender, EventArgs e)
         {
             Console.WriteLine("Inicio");
+            var valorConInterlocked = 0;
+            var valorConInterlocked2 = 0;
 
-            var valorSinInterlock = 0;
+            Parallel.For(0, 10_000, numero =>
+            {
+                Interlocked.Increment(ref valorConInterlocked);
+                Interlocked.Add(ref valorConInterlocked2, valorConInterlocked);
+            });
 
-            Parallel.For(0, 1000000, numero => valorSinInterlock++);
+            Console.WriteLine($"Valor incrementado {valorConInterlocked}");
+            Console.WriteLine($"Valor sumado {valorConInterlocked2}");
 
-            Console.WriteLine($"La sumatoria es de {valorSinInterlock}");
+            Console.WriteLine($"=====================================");
+            var valorIncrementado = 0;
+            var valorSumado = 0;
 
-            var valorConInterlock = 0;
+            var mutex = new object();
 
-            Parallel.For(0, 1000000, numero => Interlocked.Increment(ref valorConInterlock));
+            Parallel.For(0, 10_000, numero =>
+            {
+                lock (mutex)
+                {
+                    valorIncrementado++;
+                    valorSumado += valorIncrementado;
+                }
+            });
 
-            Console.WriteLine($"La sumatoria es de {valorConInterlock}");
+            Console.WriteLine($"Valor incrementado con Lock {valorIncrementado}");
+            Console.WriteLine($"Valor Sumado con Lock {valorSumado}");
+            Console.WriteLine($"=====================================");
         }
 
 
