bf2487d378cf9ada1e4a79a88b49f800d35c6611
Byron Tercero <B1tercero.cruz@gmail.com>
2023-07-19 18:13:01 -0600

    Abriendo la conexion

--- CSharp 10/Modulo 13 - ADO.NET/AdoNetApp/Program.cs
+++ CSharp 10/Modulo 13 - ADO.NET/AdoNetApp/Program.cs
@@ -5,5 +5,21 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Data;
 
-var conexionString = "Data Source=BTERCERO//SQLEXPRESS;Database=AdoNet;Integrated Security=True";
+//Variable de conexion
+var conexionString = "Data Source=BTERCERO\\SQLEXPRESS;Database=AdoNet;Integrated Security=True;TrustServerCertificate=True";
 
+//Estableciendo conexion
+try
+{
+	using (SqlConnection conexion = new SqlConnection(conexionString))
+	{
+		//abrimos la conexion
+		conexion.Open();
+        Console.WriteLine("===========Conexion Abierta===========");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Conexion fallida");
+    Console.WriteLine(ex.Message);
+}
