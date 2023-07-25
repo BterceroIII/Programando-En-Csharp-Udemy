88eb71bd7b679e7a42f1eb8cca1af4d0e585e74b
Byron Tercero <B1tercero.cruz@gmail.com>
2023-07-25 13:19:00 -0600

    Using Type and reflextion

--- CSharp 10/Modulo 14 - Reflexion/Reflexion/Program.cs
+++ CSharp 10/Modulo 14 - Reflexion/Reflexion/Program.cs
@@ -5,4 +5,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Dynamic;
 using System.Reflection;
 
-Saludador.Saludar("Felipe");
+// Crear una variable de tipo Type
+Type tipo = typeof(int);
+
+// a partir de un entero creo una variable Type
+int edad = 99;
+Type tipoDatoEdad = edad.GetType();
+
+// crear variable type con un string
+Type tipoDatoString = Type.GetType("System.Int32")!;
+//uso de propiedades de Type Name y Type IsArray dara un resultado boleano
+Console.WriteLine($"Es {tipoDatoString.Name} un Array? {tipoDatoString.IsArray}");
