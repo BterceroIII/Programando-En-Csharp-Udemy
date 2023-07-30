b2e04d78cee1194c672ab1212b126b956e7ed0c9
Byron Tercero <B1tercero.cruz@gmail.com>
2023-07-30 11:48:25 -0600

    Using code generator

--- CSharp 10/Modulo 14 - Reflexion/Reflexion/Program.cs
+++ CSharp 10/Modulo 14 - Reflexion/Reflexion/Program.cs
@@ -2,24 +2,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
-// Atributos personalizados
+// Generador de codigo
 
-Console.WriteLine($"--Puedes realizar las siguientes acciones--");
-
-foreach (var accion in Enum.GetValues<Acciones>())
-{
-    var campo = typeof(Acciones).GetField(accion.ToString());
-    var esconderAtributo = campo.GetCustomAttribute(typeof(EsconderAttribute));
-
-    if (esconderAtributo is not null)
-    {
-        var esconder = ((EsconderAttribute)esconderAtributo).Ocultar();
-
-        if (esconder)
-        {
-            continue;
-        }
-    }
-
-    Console.WriteLine(accion);
-}
+Saludador.Saludar("Hola pedazo");
